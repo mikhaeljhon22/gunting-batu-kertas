0d0ec6b0f2e124a62723194b08a0afbb8128a9f3
dettarune <learnbygoogling@gmail.com>
2025-12-04 10:42:58 +0700

    detarune: Add Joinroom

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	redisClient := configs.RedisConfig()
 
 	playRepo := repo.NewPlayRepo(redisClient)
-	playService := service.NewPlayService(*playRepo)
+	playService := service.NewPlayService(*&playRepo)
 	websocket := websocket.NewWebSocketHandler(playService)
 	http.HandleFunc("/ws", websocket.CreateRoom)
 	http.ListenAndServe(":8082", nil)
